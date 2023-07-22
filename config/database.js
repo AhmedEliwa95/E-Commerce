e09273ea5094e4f1e6144a4c497cbc5186212ea4
unknown <ahmedelewa79@gmail.com>
2023-07-22 23:42:09 +0200

    adding config to new laptop

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dbConnection = ()=>{
+    // MongoDB Connection
+    mongoose
+        .connect(process.env.DB_URI)
+        .then(conn=>{
+            console.log(`DB Connected: ${conn.connection.host}`)
+        })
+        .catch(e=>{
+            console.error(`DB Error: ${e}`);
+            process.exit(1);
+    });
+};
+
+module.exports = dbConnection
