42dcebfd78f8b7b8e4ff716145498a2bf6a6eb23
ahmed elewa <ahmedelewa79@gmail.com>
2023-09-01 20:40:38 +0300

    adding order model

---
+++ src/models/orderModel.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+
+const orderSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Order must belong to a user"],
+    },
+    cartItems: [
+      {
+        product: {
+          type: mongoose.Schema.ObjectId,
+          required: [true, "Product is required to the cartItem"],
+          ref: "Product",
+        },
+        quantity: {
+          type: Number,
+          default: 1,
+        },
+        color: String,
+        price: Number,
+      },
+    ],
+    taxPrice: {
+      type: Number,
+      default: 0,
+    },
+    shippingPrice: {
+      type: Number,
+      default: 0,
+    },
+    totalOrderPrice: {
+      type: Number,
+      default: 0,
+    },
+    paymentMethod: {
+      type: String,
+      enum: ["cash", "card"],
+      default: "cash",
+    },
+    isPaid: {
+      type: Boolean,
+      default: false,
+    },
+    paidAt: Date,
+    isDelivered: {
+      type: Boolean,
+      default: false,
+    },
+    deliveredAt: Date,
+  },
+  { timestamps: true }
+);
+
+const Order = mongoose.model("Order", orderSchema);
